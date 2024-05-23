7b2741c97be4de1682442b15e6970565c7073614
Alina-97 <mamoshebimokhathi@gmail.com>
2024-05-23 14:38:01 +0200

    added featured

--- mysite/website/views.py
+++ mysite/website/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from item.models import Category,Item
 
+from . forms import SignupForm 
+
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
@@ -11,3 +13,7 @@ def index(request):
     })
 def contact(request):
     return render (request, 'website/contact.html')
+
+def signup(request):
+    form=SignupForm
+    return render (request, 'website/signup.html')
